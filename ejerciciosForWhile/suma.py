b94fa9db5ad72cdc03e9b4f7d91b61cd64e07dc8
leninmt <ljc.montalvo@yavirac.edu.ec>
2022-12-11 21:09:12 -0500

    agrega ejercicios de FOR/WHILE

---
+++ ejerciciosForWhile/suma.py
@@ -0,0 +1,7 @@
+#cree un bucle que sume los numeros de 100 al 200
+x=100
+suma= 0
+while x<=200: 
+    suma=suma+x
+    x += 1
+print("la suma de los numeros del 100 al 200 es: ",suma)
---
+++ ejerciciosForWhile/tabla.py
@@ -0,0 +1,4 @@
+#tabla de multiplicar con ciclo FOR
+for f in range(1,13):
+    mul=5*f
+    print(f'5 x {f} = {mul}')
